9ef1f3eb79c1ea42f682760f13301d7ddd2b0579
vsisterolli <vsisterolli@gmail.com>
2025-01-25 19:04:56 -0300

    added some cses exs

---
+++ cses/initial/weird-algorithms.cpp
@@ -0,0 +1,48 @@
+// HELPFUL HEADER ONLY
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <chrono>
+#define pb push_back
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3f
+#define endl '\n'
+#define ll long long
+#define f first
+#define fin cin
+#define fout cout
+#define s second
+#define FAST cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+#define debug(x) cout << "DEBUG " << x << endl
+#define debug2(x, y) cout << "DEBUG " << x << " " << y << endl
+#define debug3(x, y, z) cout << "DEBUG " << x << " " << y << " " << z<< endl
+#define debug4(x, y, z, o) cout << "DEBUG " << x << " " << y << " " << z<< " " << o << endl
+#define all(x) x.begin(), x.end()
+#define left esquerda
+#define lb lower_bound
+#define right direita
+using namespace std;
+void setIO(string s) {
+ios_base::sync_with_stdio(0); cin.tie(0);
+freopen((s+".in").c_str(),"r",stdin);
+freopen((s+".out").c_str( ),"w",stdout);
+}
+typedef pair<ll, ll> pii;
+typedef vector<vector<char>> mat;
+typedef pair<int, string> pis;
+const ll mod = 998244353, MAXN = 2e5 + 5;
+typedef vector<int> vi;
+typedef pair<int, pair<int, int>> piii;
+// END HEADER
+
+int32_t main() {
+    long long int n;
+    cin >> n;
+
+    while(n != 1){
+        cout << n << " ";
+        if(n % 2)
+            n = n * 3 + 1;
+        else n >>= 1;
+    }
+    cout << 1 << endl;
+}
